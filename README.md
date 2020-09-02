a26dcf6ca69f8b189d7c022261e1503126c7e104
Stephen Lomangino <slomangino@flightdocs.com>
2020-09-02 15:07:10 -0400

    Updated readme, generated new certificates and solidified certificate generation script.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
+### Resources
+Initial starting point for docker compose
 https://codeburst.io/load-balancing-an-asp-net-core-web-app-using-nginx-and-docker-66753eb08204
+
+helpful openssl commands
+https://www.markbrilman.nl/2011/08/howto-convert-a-pfx-to-a-seperate-key-crt-file/
+https://www.sslshopper.com/article-most-common-openssl-commands.html
+
+How to setup the dev certificate when using Docker in development 
+https://github.com/dotnet/AspNetCore.Docs/issues/6199
+
+#### Code
+Look at the request cert
+https://stackoverflow.com/questions/777607/the-remote-certificate-is-invalid-according-to-the-validation-procedure-using
+
+Rob Rich presentation on this topic
+https://www.youtube.com/watch?v=oAf3_8k17E8
+https://github.com/robrich/https-aspnet-core-docker-deep-dive/blob/main/7.%20mkcert/HTTPSPlayground.API/Dockerfile
+
+
+### Known Bugs
+Openssl requiring certificate signing
+https://github.com/openssl/openssl/issues/1418
+
+dev certs on linux
+https://github.com/dotnet/aspnetcore/issues/7246
--- nginx-shared/gen-cert-stuff.ps1
+++ nginx-shared/gen-cert-stuff.ps1
@@ -1,33 +1,31 @@
-#!/bin/sh
-openssl req -config cert.config -new -out cert-csr.pem
-openssl x509 -req -days 3650 -extfile cert.config -extensions v3_req -in cert-csr.pem -signkey cert-key.pem -out cert.crt -CAkey rootCA.key -CA rootCA.crt -CAcreateserial -sha256
-openssl pkcs12 -export -out cert.pfx -inkey cert-key.pem -in cert.crt -password pass:TestPass123
+# Set the dev environment password to be used on the certificate.
+$TEST_PASSWORD = "TestPass123"
 
-# Generate the certificate
-New-SelfSignedCertificate -DnsName "localhost", "host.docker.internal", "127.0.0.1", "service1", "service2", "compose_nginx1_1", "compose_nginx2_1" -KeyUsage CertSign, DigitalSignature, KeyEncipherment -FriendlyName "Better ASP.NET Core HTTPS development certificate" -CertStoreLocation "Cert:\CurrentUser\My"
+$SECURE_PASSWORD = ConvertTo-SecureString -String $TEST_PASSWORD -Force -AsPlainText
 
-# Export the generated cert to a pfx file.
-# -> run
-# -> mmc.exe
-# -> add/remove snapins
-# -> certificates
-# -> Current user
-# -> find localhost cert
-# -> copy to file
-# -> export private key
-# -> export all extended properties
-# -> Password: TestPass123
-# -> cert.pfx
-# -> finish
+# Generate the certificate. 
+# New-SelfSignedCertificate needs to put the cert into a cert store
+# so we are grabbing the thumbprint off of the command and saving it to a variable so we can
+# export it and then remove it from the store.
 
+# New-SelfSignedCertificate -DnsName "localhost", "host.docker.internal", "127.0.0.1", "service1", "service2", "compose_nginx1_1" -KeyUsage CertSign, DigitalSignature, KeyEncipherment -FriendlyName ".net Core Https development certificate" -CertStoreLocation "Cert:\CurrentUser\My"
+$THUMBPRINT = (New-SelfSignedCertificate -DnsName "localhost" -KeyUsage CertSign, DigitalSignature, KeyEncipherment -FriendlyName ".net Core Https development certificate" -CertStoreLocation "Cert:\CurrentUser\My").Thumbprint
+
+# Export the certificate as a .pfx file
+Get-ChildItem -Path Cert:\CurrentUser\My\$THUMBPRINT | Export-PfxCertificate -FilePath .\cert.pfx -Password $SECURE_PASSWORD
+
+# Remove the certificate from the windows store, it doesnt need to be there
+Remove-Item -Path Cert:\CurrentUser\My\$THUMBPRINT
+
+# Perform some openssl manipulation to get the cert into the file extensions we need.
 # Extract .crt file from pfx
-openssl pkcs12 -in cert.pfx -clcerts -nokeys -out cert.crt
+openssl pkcs12 -in cert.pfx -clcerts -nokeys -out cert.crt -passin pass:$TEST_PASSWORD
 #
 # Extract encrypted private key from pfx 
-openssl pkcs12 -in cert.pfx -nocerts -out cert-key-e.key
+openssl pkcs12 -in cert.pfx -nocerts -out cert-key-encrypted.key -passin pass:$TEST_PASSWORD -passout pass:$TEST_PASSWORD
 # 
 # Decrypt private key
-openssl rsa -in cert-key-e.key -out cert-key.key
+openssl rsa -in cert-key-encrypted.key -out cert-key.key -passin pass:$TEST_PASSWORD
 # 
 # 
 # 
